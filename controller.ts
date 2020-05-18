466ad532299f7e80ca8d36de78e513fb9e51f8bd
jaydenkwright <jaydenkwright3@gmail.com>
2020-05-18 19:57:55 -0400

    Movies can now be retrieve from MongoDB collection from /movies/ route

--- controller.ts
+++ controller.ts
@@ -1,16 +1,11 @@
 import { Movie } from './models.ts'
-import { movies } from './api.ts'
+import { collection } from './db.ts'
 
 export const getMovies = async ({response} : {response: any}) => {
+    const movies = await collection.find()
     response.body = movies
 }
 
 export const addMovie = async ({ request, response} : {request: any, response: any}) => {
-    const body = await request.body()
-    const movie: Movie = body.value
-
-    movies.push(movie)
-    response.body = {movieAdded: "Sucess"}
-    response.status = 200
 
 }
--- db.ts
+++ db.ts
@@ -1,15 +1,14 @@
-import "https://deno.land/x/denv/mod.ts";
 import { init, MongoClient } from "https://deno.land/x/mongo@v0.6.0/mod.ts";
+import { host } from './host.ts'
 import { movies } from './api.ts'
 
-const host: any = Deno.env.get('DB_HOST_URL')
 await init()
 const client = new MongoClient()
-console.log(host)
-client.connectWithUri(Deno.env.get('DB_HOST_URL'))
+client.connectWithUri(host)
 
-const db = client.database("test")
-const moviesDb = db.collection("movies");
-//console.log(movies)
-const insertId = await moviesDb.insertMany(movies);
+const dbName = client.database("test")
+export const collection = dbName.collection("movies");
 
+const mov = await collection.find()
+
+console.log(mov)
